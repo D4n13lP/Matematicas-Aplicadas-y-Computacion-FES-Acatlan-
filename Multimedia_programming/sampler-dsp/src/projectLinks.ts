87a065fe49c696abd5881d0d55fd79ba41776c81
Daniel Pineda <dan137p1n3d4@gmail.com>
2026-06-27 02:38:18 -0600

    agregados links del video explicativo y enlace al repositorio de GitHub

--- Multimedia_programming/sampler-dsp/src/projectLinks.ts
+++ Multimedia_programming/sampler-dsp/src/projectLinks.ts
@@ -7,8 +7,8 @@
 // Pega aquí el enlace del video subido a YouTube.
 // Acepta cualquier formato: youtube.com/watch?v=..., youtu.be/..., shorts/...
 // Ejemplo: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-export const YOUTUBE_VIDEO_URL = ''
+export const YOUTUBE_VIDEO_URL = 'https://youtu.be/LppNbg81tPg'
 
 // Pega aquí el enlace del repositorio de GitHub del proyecto.
 // Ejemplo: 'https://github.com/tu-usuario/sampler-dsp'
-export const GITHUB_REPO_URL = ''
+export const GITHUB_REPO_URL = 'https://github.com/D4n13lP/Matematicas-Aplicadas-y-Computacion-FES-Acatlan-/tree/main/Multimedia_programming/sampler-dsp'
